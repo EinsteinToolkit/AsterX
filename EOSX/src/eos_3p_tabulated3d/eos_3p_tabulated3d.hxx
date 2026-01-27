8b7525e836c7ff4f4780626bf51a0e1dfe7267b4
Allen Wen <acw6923@g.rit.edu>
2026-01-27 15:59:24 -0600

    EOSX: communicate EOS table on CPUs, then copy to device

--- EOSX/src/eos_3p_tabulated3d/eos_3p_tabulated3d.hxx
+++ EOSX/src/eos_3p_tabulated3d/eos_3p_tabulated3d.hxx
@@ -84,18 +84,23 @@ public:
 
     const int npoints = nrho * ntemp * nye;
 
+    // Initialize host and device memory
     CCTK_REAL *logrho = (CCTK_REAL *)amrex::The_Managed_Arena()->alloc(
         nrho * sizeof(CCTK_REAL));
     CCTK_REAL *logtemp = (CCTK_REAL *)amrex::The_Managed_Arena()->alloc(
         ntemp * sizeof(CCTK_REAL));
     CCTK_REAL *yes =
         (CCTK_REAL *)amrex::The_Managed_Arena()->alloc(nye * sizeof(CCTK_REAL));
-    CCTK_REAL *alltables_tmp = (CCTK_REAL *)amrex::The_Managed_Arena()->alloc(
-        npoints * NTABLES * sizeof(CCTK_REAL));
     CCTK_REAL *alltables = (CCTK_REAL *)amrex::The_Managed_Arena()->alloc(
         npoints * NTABLES * sizeof(CCTK_REAL));
     energy_shift =
         (CCTK_REAL *)amrex::The_Managed_Arena()->alloc(sizeof(CCTK_REAL));
+    
+    CCTK_REAL *logrho_host = new CCTK_REAL[nrho];
+    CCTK_REAL *logtemp_host = new CCTK_REAL[ntemp];
+    CCTK_REAL *yes_host = new CCTK_REAL[nye];
+    CCTK_REAL *alltables_tmp = new CCTK_REAL[npoints * NTABLES];
+    CCTK_REAL *alltables_host = new CCTK_REAL[npoints * NTABLES];
 
     static const char *dnames[NTABLES] = {
         "logpress", "logenergy", "entropy", "munu", "cs2",  "dedt", "dpdrhoe",
@@ -103,9 +108,9 @@ public:
         "Xn",       "Xp",        "Abar",    "Zbar", "gamma"};
 
 #ifdef H5_HAVE_PARALLEL
-    get_hdf5_real_dset(file_id, "logrho", nrho, logrho);
-    get_hdf5_real_dset(file_id, "logtemp", ntemp, logtemp);
-    get_hdf5_real_dset(file_id, "ye", nye, yes);
+    get_hdf5_real_dset(file_id, "logrho", nrho, logrho_host);
+    get_hdf5_real_dset(file_id, "logtemp", ntemp, logtemp_host);
+    get_hdf5_real_dset(file_id, "ye", nye, yes_host);
     get_hdf5_real_dset(file_id, "energy_shift", 1, energy_shift);
     for (int iv = 0; iv < NTABLES; iv++) {
       get_hdf5_real_dset(file_id, dnames[iv], npoints,
@@ -120,17 +125,16 @@ public:
           for (int i = 0; i < nrho; i++) {
             int indold = i + nrho * (j + ntemp * (k + nye * iv));
             int indnew = iv + NTABLES * (i + nrho * (j + ntemp * k));
-            alltables[indnew] = alltables_tmp[indold];
+            alltables_host[indnew] = alltables_tmp[indold];
           }
-    amrex::The_Managed_Arena()->free(alltables_tmp);
 
     CHECK_ERROR(H5Fclose(file_id));
     CHECK_ERROR(H5Pclose(fapl_id));
 #else
     if (rank == 0) {
-      get_hdf5_real_dset(file_id, "logrho", nrho, logrho);
-      get_hdf5_real_dset(file_id, "logtemp", ntemp, logtemp);
-      get_hdf5_real_dset(file_id, "ye", nye, yes);
+      get_hdf5_real_dset(file_id, "logrho", nrho, logrho_host);
+      get_hdf5_real_dset(file_id, "logtemp", ntemp, logtemp_host);
+      get_hdf5_real_dset(file_id, "ye", nye, yes_host);
       get_hdf5_real_dset(file_id, "energy_shift", 1, energy_shift);
       for (int iv = 0; iv < NTABLES; iv++) {
         get_hdf5_real_dset(file_id, dnames[iv], npoints,
@@ -146,18 +150,32 @@ public:
             for (int i = 0; i < nrho; i++) {
               int indold = i + nrho * (j + ntemp * (k + nye * iv));
               int indnew = iv + NTABLES * (i + nrho * (j + ntemp * k));
-              alltables[indnew] = alltables_tmp[indold];
+              alltables_host[indnew] = alltables_tmp[indold];
             }
     }
-    amrex::The_Managed_Arena()->free(alltables_tmp);
 
-    MPI_Bcast(logrho, nrho, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-    MPI_Bcast(logtemp, ntemp, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-    MPI_Bcast(yes, nye, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Bcast(logrho_host, nrho, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Bcast(logtemp_host, ntemp, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Bcast(yes_host, nye, MPI_DOUBLE, 0, MPI_COMM_WORLD);
     MPI_Bcast(energy_shift, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-    MPI_Bcast(alltables, npoints * NTABLES, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Bcast(alltables_host, npoints * NTABLES, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 #endif
 
+    // Copy from host to GPU
+    cudaMemcpy(logrho, logrho_host, nrho * sizeof(CCTK_REAL), cudaMemcpyHostToDevice);
+    cudaMemcpy(logtemp, logtemp_host, ntemp * sizeof(CCTK_REAL), cudaMemcpyHostToDevice);
+    cudaMemcpy(yes, yes_host, nye * sizeof(CCTK_REAL), cudaMemcpyHostToDevice);
+    cudaMemcpy(alltables, alltables_host, npoints * NTABLES * sizeof(CCTK_REAL), cudaMemcpyHostToDevice);
+
+    cudaDeviceSynchronize();
+
+    // Free host memory
+    delete[] logrho_host;
+    delete[] logtemp_host;
+    delete[] yes_host;
+    delete[] alltables_tmp;
+    delete[] alltables_host;
+  
     *energy_shift *= EPSGF;
     const CCTK_REAL ln10 = log(10.0);
     const CCTK_REAL inv_time2 = 1 / (TIMEGF * TIMEGF);
